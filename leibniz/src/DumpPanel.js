0a763e751a891038e33a1b90267879ca05c318be
mmarini <marco.marini@mmarini.org>
2018-10-05 22:45:48 +0200

    DumpPanel

--- leibniz/src/DumpPanel.js
+++ leibniz/src/DumpPanel.js
@@ -5,22 +5,43 @@ import { saveAs } from 'file-saver/FileSaver';
 
 class DumpPanel extends Component {
 
+  constructor(props) {
+    super();
+    this.state = {
+      counts: '10',
+      dt: '0.1'
+    };
+  }
+
   dumpData() {
     var sys = this.props.result.system;
-    const dt = 1;
     if (sys) {
+      const dt = parseFloat(this.state.dt);
+      const counts = parseInt(this.state.counts);
       const dumpTable = [];
-      for (var i = 0; i < 10; i++) {
+      for (var i = 0; i < counts; i++) {
         dumpTable.push(sys.dumpWithDt(dt));
         sys = sys.next(dt);
       }
       const keys = _(dumpTable[0]).keys().sortBy().value();
       const header = _.reduce(keys, (a, b) => a + ',' + b) + '\n\r';
-      const blob = new Blob([header], { type: "text/plain;charset=utf-8" });
+      const rows = _(dumpTable).map(row =>
+        _(keys).map(k =>
+          '"' + row[k].toString() + '"').reduce((a, b) => a + ',' + b)
+      ).reduce((a, b) => a + '\n\r' + b) + '\n\r';
+      const blob = new Blob([header + rows], { type: "text/plain;charset=utf-8" });
       saveAs(blob, "dump.csv");
     }
   }
 
+  setCounts(value) {
+    this.setState({ counts: value });
+  }
+
+  setDt(value) {
+    this.setState({ dt: value });
+  }
+  
   render() {
     const dumpDisabled = !this.props.result.system;
     return (
@@ -29,12 +50,18 @@ class DumpPanel extends Component {
           <Form inline>
             <FormGroup controlId="formInlineName" bsSize="sm">
               <ControlLabel>Counts</ControlLabel>{' '}
-              <FormControl type="text" placeholder="Counts" />
+              <FormControl type="text" placeholder="Counts"
+                onInput={ev => this.setCounts(ev.target.value)}
+                onChange={() => { }}
+                value={this.state.counts} />
             </FormGroup>{' '}
             <FormGroup controlId="formInlineName" bsSize="sm">
               <ControlLabel>dt</ControlLabel>{' '}
-              <FormControl type="text" placeholder="dt" />
-            </FormGroup>
+              <FormControl type="text" placeholder="dt"
+               onInput={ev => this.setDt(ev.target.value)}
+               onChange={() => { }}
+               value={this.state.dt} />
+           </FormGroup>
             <Button bsStyle="primary"
               disabled={dumpDisabled}
               onClick={() => this.dumpData()} >Download</Button>
